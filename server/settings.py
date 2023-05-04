95075d64ee4664fe3b487ddc514951426e4d844b
Nishant Pall <palln6935@gmail.com>
2023-05-04 16:59:57 +0530

    Create user to initiate auth0

--- server/settings.py
+++ server/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import json
+from urllib import request
+from cryptography.x509 import load_pem_x509_certificate
+from cryptography.hazmat.backends import default_backend
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -73,6 +78,13 @@ TEMPLATES = [
 WSGI_APPLICATION = "server.wsgi.application"
 
 
+RES_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.isAuthenticated"),
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_jwt.authentication.JSONWebTokenAuthentication"
+    ),
+}
+
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
@@ -128,3 +140,35 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH0_DOMAIN = "dev-dxce6du8dz85qmoo.us.auth0.com"
+API_IDENTIFIER = "http://djang-vue-auth.com"
+PUBLIC_KEY = None
+JWT_ISSUER = None
+
+
+if AUTH0_DOMAIN:
+    jsonurl = request.urlopen("https://" + AUTH0_DOMAIN + "/.well-known/jwks.json")
+    jwks = json.loads(jsonurl.read().decode("utf-8"))
+    cert = (
+        "-----BEGIN CERTIFICATE-----\n"
+        + jwks["keys"][0]["x5c"][0]
+        + "\n-----END CERTIFICATE-----"
+    )
+    certificate = load_pem_x509_certificate(cert.encode("utf-8"), default_backend())
+    PUBLIC_KEY = certificate.public_key()
+    JWT_ISSUER = "https://" + AUTH0_DOMAIN + "/"
+
+
+def jwt_get_username_from_payload_handler(payload):
+    return "auth0user"
+
+
+JWT_AUTH = {
+    "JWT_PAYLOAD_GET_USERNAME_HANDLER": jwt_get_username_from_payload_handler,
+    "JWT_PUBLIC_KEY": PUBLIC_KEY,
+    "JWT_ALGORITHM": "RS256",
+    "JWT_AUDIENCE": API_IDENTIFIER,
+    "JWT_ISSUER": JWT_ISSUER,
+    "JWT_AUTH_HEADER_PREFIX": "Bearer",
+}
